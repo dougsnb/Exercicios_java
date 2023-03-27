10cf9f3b09349ffa42227f7205d4da9a0e8965de
dougsnb <126212241+dougsnb@users.noreply.github.com>
2023-03-27 09:07:30 -0300

    Add files via upload

---
+++ _lista2_exercicios_/src/ex_6_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex_6_1 {
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        int num1, num2, num3;
+
+        System.out.println("DIGITE 3 NUMEROS");
+        num1 = imput.nextInt();
+        num2 = imput.nextInt();
+        num3 = imput.nextInt();
+
+        if ((num1 < num2 + num3) && (num2 < num1 + num3) && (num3 < num1 + num2)) {
+            System.out.println("FORMA UM TRIANGULO");
+        } else {
+            System.out.println("NÃO FORMA UM TRIANGULO");
+        }
+    }
+}
